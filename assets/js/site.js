894d6faaf2425451d2a7fa348f450cdd91891d0d
mytharcher <mytharcher@gmail.com>
2012-12-11 00:19:43 +0800

    Add new feature (#2): auto load article when scrolling.

--- assets/js/site.js
+++ assets/js/site.js
@@ -9,7 +9,7 @@
 var site = {
 	InitMap: {
 		list: function () {
-			elf('article>h2').on('click', site.Handlers.loadArticle);
+			elf(window).on('scroll', site.Handlers.scrolling);
 		},
 		
 		post: function () {
@@ -27,29 +27,43 @@ var site = {
 	},
 	
 	Handlers: {
-		loadArticle: function (ev) {
-			var target = ev.target,
-				item = elf(target).parent();
-			if (target.nodeName != 'A' && item.attr('content-loaded') != 1) {
-				elf().ajax({
-					url: target.firstChild.getAttribute('href'),
-					onsuccess: site.Handlers.showAjaxContent.bind(item)
-				});
-			}
-			target = null;
+		deferLoad: function () {
+			elf('article').toArray()
+				.filter(site.Util.isViewable)
+				.filter(function (item) {
+					return item.getAttribute('content-loaded') != 1;
+				}).slice(0, 2).forEach(site.Handlers.loadArticle);
+			
 		},
 		
-		showAjaxContent: function (response) {
+		loadArticle: function (item) {
+			elf().ajax({
+				url: elf(item).firstChild().firstChild().attr('href'),
+				onsuccess: function (response) {
+					site.Handlers.showAjaxContent(item, response);
+				}
+			});
+		},
+		
+		showAjaxContent: function (node, response) {
+			var article = elf(node);
 			var content = response.split('<p class="meta">')[1].split('</p>');
 			content.shift();
 			content = content.join('</p>');
 			content = content.split(/<\/article>/)[0];
-			this.query('>.article-content').html(content);
-			this.attr('content-loaded', 1);
-			
-			this.query('pre').forEach(function (item) {
+			article.query('>.article-content').html(content);
+			article.attr('content-loaded', 1);
+			article.query('pre').forEach(function (item) {
 				hljs.highlightBlock(item);
 			});
+		},
+		
+		scrolling: function () {
+			var timer = site.scrollingTimer;
+			if (timer) {
+				clearTimeout(timer);
+			}
+			site.scrollingTimer = setTimeout(site.Handlers.deferLoad, 1000);
 		},
 		
 		onGCSEAPILoad: function () {
@@ -79,6 +93,23 @@ var site = {
 				);
 				customSearchControl.execute(query);
 			}
+		}
+	},
+	
+	Util: {
+		
+		isViewable: function (element) {
+			var pos = element.getBoundingClientRect();
+			var doc = js.dom.Stage.getDocumentElement();
+			var winHeight = doc.clientHeight;
+			var winWidth = doc.clientWidth;
+			var scrollLeft = document.body.scrollLeft || doc.scrollLeft;
+			var scrollTop = document.body.scrollTop || doc.scrollTop;
+			
+			return (pos.right > 0 &&
+				pos.left < winWidth &&
+				pos.bottom > 0 &&
+				pos.top < winHeight);
 		}
 	}
 };
